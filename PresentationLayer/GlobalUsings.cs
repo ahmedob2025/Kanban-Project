983276525d643929006a6f6ac6386d99198a7eb4
Muhammad jneed <mohammad2jniad@gmail.com>
2026-09-12 02:11:16 +0300

    fixed path for BLL layer

--- PresentationLayer/GlobalUsings.cs
+++ PresentationLayer/GlobalUsings.cs
@@ -2,11 +2,14 @@
 global using System.Data;
 global using System.Windows.Forms;
 
-// Direct access
+// Direct layer namespaces
 global using KanbanProjectManagementSystem.Common;
+global using KanbanProjectManagementSystem.Common.Entities;
 global using KanbanProjectManagementSystem.DataAccessLayer;
-global using BusinessLogicLayer;
+global using KanbanProjectManagementSystem.BusinessLogicLayer;
 
-// Aliases
+// Aliases to prevent teammate syntax breaks
 global using DataAccessLayer = KanbanProjectManagementSystem.DataAccessLayer;
+global using BusinessLogicLayer = KanbanProjectManagementSystem.BusinessLogicLayer;
 global using Common = KanbanProjectManagementSystem.Common;
+nbanProjectManagementSystem.Common;
